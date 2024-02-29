e521b69a27d91dd0391c824a0917ab1c59755912
Lukasz Gumienniczuk <01178841@pw.edu.pl>
2024-02-29 22:16:08 +0100

    setting up

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,9 +1,7 @@
 import React from "react";
-import logo from "./logo.svg";
-import "./App.css";
 
 function App() {
-  return <div className="App"></div>;
+  return <main className="bg-amber-100"></main>;
 }
 
 export default App;
