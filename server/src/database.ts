8683983857a99146275eb4f88c0fa7bcd9ce8bd4
Lukasz Gumienniczuk <01178841@pw.edu.pl>
2024-03-10 21:00:21 +0100

    database connection, and some tests with queries

--- server/src/database.ts
+++ server/src/database.ts
@@ -1,17 +1,47 @@
 import dotenv from "dotenv";
 import mysql from "mysql2";
+import { create } from "domain";
 
 dotenv.config({ path: ".env" });
 
 // singleton dla klasy database connection
+export class Database {
+  private static instance: Database;
+  private connection;
 
-export const db = mysql
-  .createConnection({
-    host: process.env.DATABASE_HOST,
-    user: process.env.DATABASE_USER,
-    password: process.env.DATABASE_PASSWORD,
-    database: process.env.DATABASE,
-  })
-  .promise();
+  private constructor() {
+    this.connection = this.createConnection();
+  }
 
-console.log(process.env.DATABASE_HOST);
+  public static getInstance() {
+    if (this.instance == null) {
+      this.instance = new Database();
+    }
+    return this.instance;
+  }
+
+  private createConnection() {
+    const connection = mysql.createConnection({
+      host: process.env.DATABASE_HOST,
+      user: process.env.DATABASE_USER,
+      password: process.env.DATABASE_PASSWORD,
+      database: process.env.DATABASE,
+    });
+    connection.connect((err) => {
+      if (err) {
+        throw new Error(`Database connection error ${err}`);
+      } else {
+        console.log(`Connected to database ${process.env.DATABASE}`);
+      }
+    });
+    return connection.promise();
+  }
+  public async getUsers(): Promise<Object> {
+    try {
+      const query = await this.connection.query("Select * from Users");
+      return query;
+    } catch (err) {
+      console.error("xd");
+    }
+  }
+}
--- server/src/server.ts
+++ server/src/server.ts
@@ -1,10 +1,18 @@
 import express, { Express, Request, Response } from "express";
 import { router as authRoute } from "./routes/auth";
-import { db } from "./database";
+import { Database } from "./database";
 const app = express();
 
 app.use(express.json());
 
 app.use("/api/user", authRoute);
+
+const db = Database.getInstance();
+
+async function users() {
+  const users = await db.getUsers();
+  console.log(users[1]);
+}
+users();
 
 app.listen(5000, () => console.log("Server running on port 5000"));
